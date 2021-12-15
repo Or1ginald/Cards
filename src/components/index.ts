89e83b27f56ee97520624d754d3fd62c6f81cb3a
Or1gianld <superhd@icloud.com>
2021-12-15 19:09:45 +0300

    Fix import

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export { CustomButton } from './customButton';
 export { CustomCheckbox } from './customCheckBox';
 export { CustomInput } from './customInput';
 export { Login } from './loginPage';
-export { ConfirmPassword } from './confirmPassword';
 export * from './confirmPassword';
