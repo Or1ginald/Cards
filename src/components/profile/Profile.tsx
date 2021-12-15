ba04b36947c07ab4b325822dcb2237047eaa8f39
Elena176 <lena.tdn176@gmail.com>
2021-12-15 19:29:04 +0300

    added Profile

---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ReturnComponentType } from '../../types';
+
+export const Profile: React.FC<ProfilePropsType> = (): ReturnComponentType => (
+  <div>
+    <h1> Profile </h1>
+  </div>
+);
+
+export type ProfilePropsType = {};
