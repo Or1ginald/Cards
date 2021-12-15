114b3da749100558bbcdd9268d8a7dd81e6da13d
Or1gianld <superhd@icloud.com>
2021-12-15 19:29:24 +0300

    Fix instance import

--- src/api/forgotPasswordApi.ts
+++ src/api/forgotPasswordApi.ts
@@ -1,8 +1,8 @@
-import axios, { AxiosResponse } from 'axios';
+import { AxiosResponse } from 'axios';
 
 import { Nullable } from '../types';
 
-/* import { instance } from './apiConfig'; */
+import { instance } from './apiConfig';
 
 export type AddNewPassType = {
   email: Nullable<string>;
@@ -14,13 +14,6 @@ export type SetNewPassType = {
   password: Nullable<string>;
   resetPasswordToken: Nullable<string>;
 };
-
-const baseUrl = process.env.REACT_APP_BASE_URL; // Достаем базовый юрл из .env(ссылка на хироку)
-
-export const instance = axios.create({
-  baseURL: baseUrl,
-  withCredentials: true,
-});
 
 export const addNewPassAPI = {
   addNewPass(params: AddNewPassType) {
