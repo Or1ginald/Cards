f89ed038ba359803d877c1c33be742ae808d082b
kitty-sam <www.moonlight@mail.ru>
2021-12-17 18:58:25 +0300

    refactor path heroku

--- src/components/confirmPassword/ConfirmPassword.tsx
+++ src/components/confirmPassword/ConfirmPassword.tsx
@@ -28,7 +28,7 @@ padding: 15px;
 border-color: #ff9900; 
 width: 250px;
 height: 30px'> 
-password recovery link: <a href='http://localhost:3000/#/createNewPassword/$token$'> recovery link </a></div>`,
+password recovery link: <a href='https://neko-back.herokuapp.com/2.0/#/createNewPassword/$token$'> recovery link </a></div>`,
   };
 
   const onSendButtonClick = (): void => {
