4a3997d21e0fb128aadb1e60978ed4c494593ef6
Or1gianld <superhd@icloud.com>
2021-12-20 16:24:04 +0300

    Fix path

--- src/enum/pathes.ts
+++ src/enum/pathes.ts
@@ -2,7 +2,7 @@ export enum PATH {
   REGISTRATION = '/registration',
   LOGIN = '/login',
   PROFILE = '/profile',
-  CONFIRM_PASSWORD = '/confirmPassword',
+  CONFIRM_PASSWORD = '/confirmPassword/*',
   POPUP = '/popup',
   CREATE_NEW_PASSWORD = '/createNewPassword',
   PAGE_404 = '/*',
