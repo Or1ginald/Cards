f77990f356f0bdf77f991ad91b16c6825c9329a0
Or1gianld <superhd@icloud.com>
2021-12-20 15:38:23 +0300

    Pagination component template

---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,8 @@
+import { ReturnComponentType } from 'types';
+
+type PaginationPropsType = {};
+
+export const Pagination = (props: PaginationPropsType): ReturnComponentType => {
+  const {} = props;
+  return <div>1</div>;
+};
