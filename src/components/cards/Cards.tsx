9f78d1f3f1e2ca464064b3049f870441100b1434
Elena176 <lena.tdn176@gmail.com>
2021-12-24 17:44:22 +0300

    refactor cards

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -85,23 +85,3 @@ export const Cards = (): ReturnComponentType => {
     </div>
   );
 };
-
-/* const res = arr.map(item => (
-  <tr key={item.id}>
-    <td>{item.name}</td>
-    <td>{item.price}</td>
-  </tr>
-)); */
-/*
-  <table className={s.container}>
-    <thead>
-      <tr>
-        <td>Question</td>
-        <td>Answer</td>
-        <td>Last Updated</td>
-        <td>Grade</td>
-        <td>Actions</td>
-      </tr>
-    </thead>
-    <tbody>{}</tbody>
-  </table> */
